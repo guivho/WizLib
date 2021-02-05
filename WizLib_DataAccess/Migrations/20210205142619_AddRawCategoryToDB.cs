2ead8139fcc3ceb1c4814cab5cd5dccbb90e6d11
Guido Van Hoecke <guivho@gmail.com>
2021-02-05 15:36:28 +0100

    39 Adding initial data with migration

---
+++ WizLib_DataAccess/Migrations/20210205142619_AddRawCategoryToDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class AddRawCategoryToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //manually inserted statements
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cay 1')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cay 2')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cay 3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
