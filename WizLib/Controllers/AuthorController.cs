da9c3ae649063995ef2fdccd8a75c54a0d9d620f
Guido Van Hoecke <guivho@gmail.com>
2021-02-06 16:35:20 +0100

    46 Assignment Author and Publisher

---
+++ WizLib/Controllers/AuthorController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AuthorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Author> authorList = _db.Authors.ToList<Author>();
+            return View(authorList);
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            var author = new Author();
+            if (id != null)
+            {
+                author = _db.Authors.FirstOrDefault(c => c.AuthorId == id);
+                if (author == null)
+                {
+                    return NotFound();
+                }
+            }
+            return View(author);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                if (author.AuthorId > 0)
+                {
+                    _db.Authors.Update(author);
+                }
+                else
+                {
+                    _db.Authors.Add(author);
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            var author = _db.Authors.FirstOrDefault(c => c.AuthorId == id);
+            if (author != null)
+            {
+                _db.Authors.Remove(author);
+                _db.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WizLib/Controllers/PublisherController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PublisherController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Publisher> categoryList = _db.Publishers.ToList<Publisher>();
+            return View(categoryList);
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            var category = new Publisher();
+            if (id != null)
+            {
+                category = _db.Publishers.FirstOrDefault(c => c.PublisherId == id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Publisher category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.PublisherId > 0)
+                {
+                    _db.Publishers.Update(category);
+                }
+                else
+                {
+                    _db.Publishers.Add(category);
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            var category = _db.Publishers.FirstOrDefault(c => c.PublisherId == id);
+            if (category != null)
+            {
+                _db.Publishers.Remove(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
